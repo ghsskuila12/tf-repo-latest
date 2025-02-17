9457e8388ae495111a7f9d5bba0cfb6b9b886b30
sudhansusekhar kuila <skuila@enlil.com>
2025-02-17 16:58:30 +0530

    added inital code

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "azs" {
+  description = "List of Availability Zones"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+}
+
